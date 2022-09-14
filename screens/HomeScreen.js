2abcb6a6ed7bfb5861675b2b0d161739569cc254
intersoom <sallybig@naver.com>
2022-09-14 23:17:04 +0900

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,32 @@
+import React, {useEffect} from 'react';
+import {View, Button} from 'react-native';
+
+function HomeScreen({navigation}) {
+  useEffect(() => {
+    navigation.setOptions({title: '홈'});
+  }, [navigation]);
+  return (
+    <View>
+      <Button
+        title="Detail1 열기"
+        onPress={() => {
+          navigation.push('Detail', {id: 1});
+        }}
+      />
+      <Button
+        title="Detail2 열기"
+        onPress={() => {
+          navigation.push('Detail', {id: 2});
+        }}
+      />
+      <Button
+        title="Detail3 열기"
+        onPress={() => {
+          navigation.push('Detail', {id: 3});
+        }}
+      />
+    </View>
+  );
+}
+
+export default HomeScreen;
